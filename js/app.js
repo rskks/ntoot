cd902c5fe95b498cb15a30b7b55b59edb50fdc55
rskks <ras.keks@gmail.com>
2023-04-24 23:08:57 -0500

    v0.8 all code from video + played with gallery

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#pagepiling').pagepiling({
         menu:null,
         direction:'vertical',
-        sectionsColor: ['lightsalmon','antiquewhite','white','olivedrab','aliceblue'],
+        sectionsColor: ['#F3E2B7','antiquewhite','white','antiquewhite','aliceblue'],
         anchors:['home-sec','about-sec','project-sec','testimony-sec','contact-sec'],
         menu:'#myMenu',
         scrollingSpeed:700,
@@ -12,8 +12,13 @@ $(document).ready(function() {
     // filterizr
 
     var filterizr = $('.filter-container').filterizr({
-        animationDuration: .5
-    })
+        animationDuration: .5,
+        layout: 'packed',
+        grid: {
+          rows: 0, // auto-calculate number of rows based on items count
+          columns: 5 // number of columns
+        }
+    });
 
     // Slick slider
     $('.slider').slick({
@@ -33,6 +38,11 @@ $(document).ready(function() {
                 }
             }
         ]
+    });
+
+    //mobile menu
+    $('.burger').on('click',function(){
+        $('.overlay').toggleClass('mob-active');
     })
 });
 
