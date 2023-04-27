81c71272deec228effa8cf37fee3c2399bab9dc0
rskks <ras.keks@gmail.com>
2023-04-26 21:20:44 -0500

    v0.92 lightbox added, not fixed

--- js/app.js
+++ js/app.js
@@ -6,17 +6,61 @@ $(document).ready(function() {
         easing:'swing',
         scrollingSpeed: 751,
         touchSensitivity: 10,
+        navigation:true,
     });
 
     // filterizr
 
-    var filterizr = $('.filter-container').filterizr({
-        animationDuration: .5,
-        layout: 'packed',
-        grid: {
-          rows: 2, // auto-calculate number of rows based on items count // number of columns
-        }
+ //   var filterizr = $('.filter-container').filterizr({
+ //       animationDuration: .5,
+ //      layout: 'packed',
+ //       grid: {
+ //         rows: 2, // auto-calculate number of rows based on items count // number of columns
+ //       }
+ //   });
+
+ //   $('a[data-lightbox]').filter(function() {
+ //       return !$(this).hasClass('filteredOut');
+ //     }).lightbox();
+
+// filterizr initialization
+
+var filterizr = $('.filter-container').filterizr({
+    animationDuration: .5,
+    layout: 'packed',
+    grid: {
+      rows: 2
+    }
+  });
+
+
+// lightbox
+$(document).ready(function() {
+    // Initialize lightbox with only the filtered images
+    var $lightboxItems = $('a[data-lightbox]');
+    $lightboxItems.lightbox({
+      'disableScrolling': true,
+      'alwaysShowNavOnTouchDevices': true,
+      'positionFromTop': 50,
+      'wrapAround': true
     });
+      
+    // Filter the lightbox items
+    $('.filter-container').on('filtering.filterizr', function() {
+      var $filteredItems = $lightboxItems.filterizr('getFiltered');
+      $lightboxItems.not($filteredItems).data('lightbox', null);
+      $filteredItems.lightbox({
+        'disableScrolling': true,
+        'alwaysShowNavOnTouchDevices': true,
+        'positionFromTop': 50,
+        'wrapAround': true
+      });
+    });
+  });
+  
+
+
+      
 
     // Slick slider
     $('.slider').slick({
